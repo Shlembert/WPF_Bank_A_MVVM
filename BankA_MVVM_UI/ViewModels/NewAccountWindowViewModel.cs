f778cf8cc0371afa57d98653614ded0f98086e62
Matin <shlembert@yandex.ru>
2024-06-14 09:04:56 +0300

    add account

--- BankA_MVVM_UI/ViewModels/NewAccountWindowViewModel.cs
+++ BankA_MVVM_UI/ViewModels/NewAccountWindowViewModel.cs
@@ -1,96 +1,57 @@
-﻿using BankA_MVVM_Library.Models;
-using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Input;
+﻿using System;
+using System.ComponentModel;
+using BankA_MVVM_Library.Models;
 
-namespace BankA_MVVM.ViewModels
+namespace BankA_MVVM_UI.ViewModels
 {
-    public class NewAccountWindowViewModel : ViewModelBase
+    public class NewAccountWindowViewModel : INotifyPropertyChanged
     {
-        private string _accountNumber;
-        public string AccountNumber
-        {
-            get => _accountNumber;
-            set
-            {
-                _accountNumber = value;
-                OnPropertyChanged();
-            }
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<Account> AccountAdded; // Добавляем событие AccountAdded
 
         private bool _isDepositAccount;
         public bool IsDepositAccount
         {
-            get => _isDepositAccount;
+            get { return _isDepositAccount; }
             set
             {
-                _isDepositAccount = value;
-                OnPropertyChanged();
+                if (_isDepositAccount != value)
+                {
+                    _isDepositAccount = value;
+                    OnPropertyChanged(nameof(IsDepositAccount));
+                    OnPropertyChanged(nameof(IsNonDepositAccount));
+                }
             }
         }
 
-        public ICommand GenerateAccountNumberCommand { get; }
-        public ICommand DoneCommand { get; }
-        public ICommand CancelCommand { get; }
-
-        public event EventHandler<Account> AccountAdded;
-
-        private readonly Client _client;
+        public bool IsNonDepositAccount
+        {
+            get { return !_isDepositAccount; }
+            set
+            {
+                if (_isDepositAccount == value)
+                {
+                    _isDepositAccount = !value;
+                    OnPropertyChanged(nameof(IsDepositAccount));
+                    OnPropertyChanged(nameof(IsNonDepositAccount));
+                }
+            }
+        }
 
         public NewAccountWindowViewModel(Client client)
         {
-            _client = client;
-            GenerateAccountNumberCommand = new RelayCommand(GenerateAccountNumber);
-            DoneCommand = new RelayCommand(Done);
-            CancelCommand = new RelayCommand(Cancel);
+            // Your existing initialization code for client
         }
 
-        private void GenerateAccountNumber()
+        protected void OnPropertyChanged(string propertyName)
         {
-            Random random = new Random();
-            AccountNumber = random.Next(100000000, 999999999).ToString();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private void Done()
+        // Метод для вызова события AccountAdded
+        private void OnAccountAdded(Account account)
         {
-            if (string.IsNullOrEmpty(AccountNumber))
-            {
-                MessageBox.Show("Сначала сгенерируйте номер счета.");
-                return;
-            }
-
-            string accountNumber = AccountNumber.Replace(" ", "");
-            decimal initialBalance = 0;
-
-            Account newAccount = null;
-            if (IsDepositAccount)
-            {
-                newAccount = new DepositAccount(int.Parse(accountNumber), "Депозитный", initialBalance);
-            }
-            else
-            {
-                newAccount = new NonDepositAccount(int.Parse(accountNumber), "Недепозитный", initialBalance);
-            }
-
-            if (newAccount != null)
-            {
-                _client.Accounts.Add(newAccount);
-                AccountAdded?.Invoke(this, newAccount);
-                LogOperation("Создан новый счет", newAccount);
-            }
-            Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive)?.Close();
-        }
-
-        private void Cancel()
-        {
-            Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive)?.Close();
-        }
-
-        private void LogOperation(string operation, Account account)
-        {
-            string logMessage = $"{DateTime.Now}: {operation} - Счет №{account.AccountNumber}, Баланс: {account.Balance}";
-            // Реализуйте ваш механизм логирования здесь
+            AccountAdded?.Invoke(this, account);
         }
     }
 }
